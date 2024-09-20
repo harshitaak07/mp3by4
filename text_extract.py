275c58f077978d2d31c9123d9152227d9ea38dba
eshita241 <eshitachokhani2003@gmail.com>
2024-09-20 17:23:19 +0530

    feat: text-extraction

--- text_extract.py
+++ text_extract.py
@@ -1,26 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 
-#using beautiful souo for text extraction
-
 def extract_text_from_url(url):
     response = requests.get(url)
     
     if response.status_code != 200:
         return f"Failed. Status code: {response.status_code}"
     
-    soup = BeautifulSoup(response.content, 'html.parser')
+    soup = BeautifulSoup(response.content, 'html5lib')
     
-    for script_or_style in soup(['script', 'style', 'noscript', 'header', 'footer', 'aside', 'nav']):#removing styles of the web oage
-        script_or_style.decompose() 
+    for script_or_style in soup(['script', 'style', 'noscript', 'header', 'footer', 'aside', 'nav']):#removing styles/css
+        script_or_style.decompose()
 
     text_elements = []
-    for tag in soup.find_all(['h1','p', 'h2', 'h3', 'h4', 'div','h5','h6']):#only from mentioned tags
+    for tag in soup.find_all(['h1', 'p', 'h2', 'h3', 'h4', 'div', 'h5', 'h6']):
         text = tag.get_text(strip=True)
-        if text:  
-            text_elements.append(text)
+        if text:
+            text_elements.append(text + '\n') 
 
-    full_text = ' '.join(text_elements)
+    full_text = '\n\n'.join(text_elements)
     
     return full_text
 
